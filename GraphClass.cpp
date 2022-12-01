b3d71c4da9db4119a5cf5801235bfde5ea258bc6
angeliicp <115087590+angeliicp@users.noreply.github.com>
2022-12-01 22:45:12 +0700

    add comments

--- GraphClass.cpp
+++ GraphClass.cpp
@@ -3,16 +3,20 @@ class Graph{
 	vector< vector<int> > adjList;
 	
 	public:
+	//graph constructor
 	Graph(int numVertices);
+	//graph functions
 	void addEdge(int u, int v);
 	void BFS(int src);
 };
 
+//constructor
 Graph::Graph(int numVertices){
 	this->numVertices = numVertices;
 	adjList.resize(numVertices);
 }
 
+//function to add edge
 void Graph::addEdge(int u, int v){
 	adjList[u].pb(v);
 }
